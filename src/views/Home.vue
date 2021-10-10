47d4d5a1d5d3f5cadd8c539ac69a3fb6a6847302
Michal Bazych <bazych.michal@gmail.com>
2021-10-10 22:19:25 +0200

    notifications test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
       <h2
         class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
       >
-       Hello FoodDash !!!! I'm here after commit!
+       Hello FoodDash !
         <br />
         <span class="text-indigo-600">Vite + Vue 3 + Tailwind CSS</span>
       </h2>
