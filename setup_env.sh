30f93773301a55fc77a94ff2afa966ba027677c1
Michał Bazych <bazychm@gmail.com>
2021-12-18 08:57:04 +0100

    edit gitlab cicd

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH; then
+if "$CI_COMMIT_BRANCH" == "$CI_DEFAULT_BRANCH"; then
     cat $ENV_FILE_PROD > .env
 else
     cat $ENV_FILE_DEV > .env
